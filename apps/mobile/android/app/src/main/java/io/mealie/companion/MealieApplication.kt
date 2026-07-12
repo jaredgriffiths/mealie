170262b6789ac258cbf0846c1d6833e1c8aff303
jaredgriffiths <51748776+jaredgriffiths@users.noreply.github.com>
2026-07-12 14:38:45 +1000

    chore: update Mealie code

--- apps/mobile/android/app/src/main/java/io/mealie/companion/MealieApplication.kt
+++ apps/mobile/android/app/src/main/java/io/mealie/companion/MealieApplication.kt
@@ -1,7 +1,20 @@
 package io.mealie.companion
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
+import okhttp3.OkHttpClient
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MealieApplication : Application()
+class MealieApplication : Application(), ImageLoaderFactory {
+    @Inject
+    lateinit var okHttpClient: OkHttpClient
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .okHttpClient(okHttpClient)
+            .build()
+    }
+}
